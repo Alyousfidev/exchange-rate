09e9ad7bfea9eb56f3305803ab8b5e4d0b06d00a
Ameen Alyousfi <ameenahmad2010@gmail.com>
2025-02-05 22:25:15 +0300

    added free file

---
+++ pluging.js
@@ -0,0 +1,19 @@
+
+let usd = document.querySelector(".usd")
+let yer = document.querySelector(".yer")
+let btnChange= document.querySelector(".btn-change")
+let boxWraper= document.querySelector(".wraper")
+
+
+btnChange.addEventListener("click",function(){
+    boxWraper.classList.toggle("active")
+    yer.classList.toggle("active")
+    usd.classList.toggle("active")
+})
+
+yer.addEventListener("input", function(){
+    usd.value=((yer.value /530)).toFixed(2)
+})
+usd.addEventListener("input", function(){
+    yer.value=((usd.value * 530)) 
+})
